66ce6f6e52c5b1814bcdcf57f22875477ed62d8d
David Currie <sillymudder@gmail.com>
2023-10-09 09:36:30 +1100

    Added missing files.

---
+++ src/components/index.tsx
@@ -0,0 +1,13 @@
+export const MiniParagraph = () => {
+  return (
+    <div className="flex w-1/4 self-end pb-0.5 text-left text-[3px] uppercase leading-[0.2rem]">
+      <div className="w-1/2"></div>
+      <div className="w-1/2">
+        <p>Polycarbon cabal</p>
+        <p>jockey edge fieldstrip</p>
+        <p>hardwired</p>
+        <p>silverhands jackass</p>
+      </div>
+    </div>
+  );
+};
